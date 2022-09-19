fc38b717dcfcbe866d8a822b6757012e09c60a14
Uszczi <mateuszpapuga24@gmail.com>
2022-09-19 13:14:13 +0200

    w

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,37 @@
 <template>
-  <div>
-    <v-app-bar color="primary" prominent>
-      <v-app-bar-nav-icon
-        variant="text"
-        @click.stop="drawer = !drawer"
-      ></v-app-bar-nav-icon>
+  <v-card>
+    <v-layout>
+      <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
 
-      <v-toolbar-title>My files</v-toolbar-title>
+      <v-app-bar color="primary" prominent>
+        <v-app-bar-nav-icon
+          variant="text"
+          @click.stop="drawer = !drawer"
+        ></v-app-bar-nav-icon>
 
-      <v-spacer></v-spacer>
+        <v-toolbar-title>My files</v-toolbar-title>
 
-      <v-btn variant="text" icon="mdi-magnify"></v-btn>
+        <v-spacer></v-spacer>
 
-      <v-btn variant="text" icon="mdi-filter"></v-btn>
+        <v-btn variant="text" icon="mdi-magnify"></v-btn>
 
-      <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
-    </v-app-bar>
+        <v-btn variant="text" icon="mdi-filter"></v-btn>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      bottom
-      temporary
-      style="
-        max-width: 200px;
-        flex-direction: row;
-        justify-content: center;
-        align-items: flex-start;
-      "
-    >
-      <v-list :items="items"></v-list>
-    </v-navigation-drawer>
+        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+      </v-app-bar>
 
-    <MoviesTableVue />
-  </div>
+      <v-navigation-drawer v-model="drawer" bottom temporary>
+        <v-list :items="items"></v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+        <v-card-text>
+          The navigation drawer will appear from the bottom on smaller size
+          screens.
+        </v-card-text>
+      </v-main>
+    </v-layout>
+  </v-card>
 </template>
 
 <script lang="ts">
