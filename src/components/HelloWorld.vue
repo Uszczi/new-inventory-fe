2b1a0ae1b4bd508dca818f8193a60e772799b582
Uszczi <mateuszpapuga24@gmail.com>
2022-09-19 13:21:29 +0200

    Revert "w"
    This reverts commit fc38b717dcfcbe866d8a822b6757012e09c60a14.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,38 @@
 <template>
-  <v-card>
-    <v-layout>
-      <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
+  <div>
+    <v-app-bar color="primary" prominent>
+      <v-app-bar-nav-icon
+        variant="text"
+        @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon>
 
-      <v-app-bar color="primary" prominent>
-        <v-app-bar-nav-icon
-          variant="text"
-          @click.stop="drawer = !drawer"
-        ></v-app-bar-nav-icon>
+      <v-toolbar-title>My files</v-toolbar-title>
 
-        <v-toolbar-title>My files</v-toolbar-title>
+      <v-spacer></v-spacer>
 
-        <v-spacer></v-spacer>
+      <v-btn variant="text" icon="mdi-magnify"></v-btn>
 
-        <v-btn variant="text" icon="mdi-magnify"></v-btn>
+      <v-btn variant="text" icon="mdi-filter"></v-btn>
 
-        <v-btn variant="text" icon="mdi-filter"></v-btn>
+      <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+    </v-app-bar>
 
-        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
-      </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      bottom
+      temporary
+      style="
+        max-width: 200px;
+        flex-direction: row;
+        justify-content: center;
+        align-items: flex-start;
+      "
+    >
+      <v-list :items="items"></v-list>
+    </v-navigation-drawer>
 
-      <v-navigation-drawer v-model="drawer" bottom temporary>
-        <v-list :items="items"></v-list>
-      </v-navigation-drawer>
-
-      <v-main>
-        <v-card-text>
-          The navigation drawer will appear from the bottom on smaller size
-          screens.
-        </v-card-text>
-      </v-main>
-    </v-layout>
-  </v-card>
+    <MoviesTableVue />
+  </div>
 </template>
 
 <script lang="ts">
